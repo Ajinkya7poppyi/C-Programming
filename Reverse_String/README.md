6f6a23a463fd373f1d9e1aff3c1a7bf60e6adb3f
Ajinkya Patil <san.avpatil@gmail.com>
2020-05-18 12:07:14 +0530

    Reverse String In C

---
+++ Reverse_String/README.md
@@ -0,0 +1,11 @@
+***C Program To Calculate Length of String***
+
+***Author:- Ajinkya Patil***
+
+***Github:- https://github.com/Ajinkya7poppyi***
+
+**Reverse number is a number in which digits are reversed**
+
+For example, 9782 is a number. The reverse of the given number is 2879 i.e. first digit becomes last and last becomes the first reversing the order of digits.
+
+Implementation of this algorithm is implemented in two different ways:
---
+++ Reverse_String/reverse_string_approach1.c
@@ -0,0 +1,99 @@
+// C Program To Reverse A String Approach1
+// Author:- Ajinkya Patil
+// Github:- https://github.com/Ajinkya7poppyi/
+
+#include<stdio.h>
+/*define true and false*/
+#define false 0
+#define true 1
+typedef int bool;
+
+/*Calculate power of number*/
+int power(int in_number, int raise_to)
+{
+    int count_idx, power_output = 1;
+
+    /*Multiply raise_to number of times to in_number*/
+    for (count_idx = 1; count_idx <= raise_to; count_idx++)
+    {
+        power_output = power_output * in_number;
+    }
+
+    /*Return Power output*/
+    return power_output;
+}
+
+/*Return number of digits in number*/
+size_t number_digits(int input_num)
+{
+    /*Initialize count for digits*/
+    size_t count=0;
+
+    /*loop until number becomes zero*/
+    while(input_num)
+    {
+        /*Divide number by 10*/
+        input_num = input_num/10;
+        /*increment count by 1*/
+        count++;
+    }
+
+    /*return count of digits*/
+    return count;
+}
+
+/*Reverse the digits of a number*/
+int reverse_digits(int num)
+{
+    int digit, reverse_num=0;
+
+    /*Get number of digits in number*/
+    size_t digit_count = number_digits(num);
+
+    /*Take every digit and multiply with 10 factor raised to power of digit count and add to the sum*/
+    while (digit_count)
+    {
+        digit = num%10;
+        reverse_num = reverse_num + (power(10, digit_count-1) * digit);
+        num = num/10;
+        digit_count--;
+    }
+
+    /*return reversed number*/
+    return reverse_num;
+}
+
+/*Main loop of program*/
+int main()
+{
+    /*Input variable*/
+    int input_num;
+
+    char state_continue;
+
+    while(true)
+    {
+        /*Get input from user*/
+        printf("Reverse Digits of A Number\n");
+        printf("Please Enter An Integer: ");
+        scanf(" %d", &input_num);
+
+        /*Reverse the digits in number*/
+        int num_rev = reverse_digits(input_num);
+
+        /*Print reversed number*/
+        printf("Reverse Value of %d is %d\n", input_num, num_rev);
+
+        printf("Do You Want To Continue(Y/N):");
+        /*Blank space before scanf tells compiler to skip leading whitespace.*/
+        scanf(" %c", &state_continue);
+
+        /*Check if user wants to exit*/
+        if((state_continue == 'N') || (state_continue == 'n'))
+        {
+            break;
+        }
+    }
+
+   return 0;
+}
